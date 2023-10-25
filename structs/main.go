b33a19cba84d0c69a9f2a43ded685ddb4acfac82
enginearn <enginyaaaaan@gmail.com>
2023-10-26 01:41:30 +0900

    Modified

--- structs/main.go
+++ structs/main.go
@@ -14,6 +14,21 @@ type User struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
+var s = "seven"
+
+func saySomething(s string) (string, string) {
+	log.Println("s from the saySomething fnc is", s)
+	return s, "world"
+}
+
+type myStruct struct {
+	FirstName string
+}
+
+func (m *myStruct) printFirstName() string {
+	return m.FirstName
+}
+
 func main() {
 	user1 := User{
 		ID:        1,
@@ -31,4 +46,40 @@ func main() {
 	log.Println("user1 Email: ", user1.Email)
 	log.Println("user1 Created At: ", user1.CreatedAt)
 	log.Println("user1 Updated At: ", user1.UpdatedAt)
+
+	user2 := User{
+		ID:        2,
+		FirstName: "Jane",
+		LastName:  "Doe",
+		Email:     "jane_doe@example.com",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	log.Println("user2: ", user2)
+	log.Println("user2 ID: ", user2.ID)
+	log.Println("user2 First Name: ", user2.FirstName)
+	log.Println("user2 Last Name: ", user2.LastName)
+	log.Println("user2 Email: ", user2.Email)
+	log.Println("user2 Created At: ", user2.CreatedAt)
+	log.Println("user2 Updated At: ", user2.UpdatedAt)
+
+	var s2 = "six"
+
+	s := "five"
+
+	log.Println("s: ", s)
+	log.Println("s2: ", s2)
+
+	saySomething("hello")
+
+	var ms myStruct
+	ms.FirstName = "John"
+
+	myVar2 := myStruct{
+		FirstName: "Jane",
+	}
+
+	log.Println("ms: ", ms.printFirstName())
+	log.Println("ymVar2: ", myVar2.printFirstName())
 }
